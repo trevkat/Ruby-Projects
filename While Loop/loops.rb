1fa0b769714aefb5f0a07d02e1a459ef94ebe426
Trevor Katilius <111081073+trevkat@users.noreply.github.com>
2024-01-22 13:51:01 -0600

    Create loops.rb

---
+++ While Loop/loops.rb
@@ -0,0 +1,17 @@
+i = 0
+until i >= 10 do
+ puts "i is #{i}"
+ i += 1
+end
+
+for i in 0..5
+  puts "#{i} zombies incoming!"
+end
+
+5.times do |number|
+  puts "Alternative fact number #{number}"
+end
+
+5.times do
+  puts "Hello, world!"
+end
